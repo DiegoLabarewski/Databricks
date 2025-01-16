9592f00bdf58070602d874e7001add14db2d1e69
DiegoLabarewski <DiegoLabarewski>
2025-01-16 16:21:31 +0000

    teste

---
+++ Untitled Notebook 2025-01-16 13_20_27.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+df = spark.read.csv("file:/Workspace/Users/user-ufpvolullmkq@oreilly-cloudlabs.com/Databricks/circuits.csv", header=True, inferSchema=True)
+display(df)
